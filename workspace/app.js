9b4e945754a47c8a376de5ecdd2df18d88e115c6
nairuz@aspire.is <nairuz.abulhul@gmail.com>
2016-07-30 15:58:19 -0700

    if the new data exists in the database, it will update automatically, else will create a new entry

--- workspace/app.js
+++ workspace/app.js
@@ -6,7 +6,9 @@ var express    = require("express"),
     
 
 //Mongo Settings:
-mongoose.connect("mongodb://localhost/database") //database path
+mongoose.connect("mongodb://localhost/DB4") //database path
+
+// var db4 = mongoose.connect("mongodb://localhost/DB4") //database path
 
 //App settings:
 app.set("view engine","ejs");
@@ -16,7 +18,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 //Mongodb Schema
 var formSchema = new mongoose.Schema({
-    
+  user_id:String,
    firstName:String,
    lastName:String,
    address:String,
@@ -33,41 +35,86 @@ var Form = mongoose.model("Form", formSchema);
 
 //GET
 app.get("/", function(req,res){
+    // var ObjectId = "57977c851beba4e113524b0c";
   res.render("index") //renders the index page
-});
+ });
 
 
 
-//POST
+//POST ROUTE
 app.post('/', function(req, res) {
 
     //Extracting data from the form input
-     var firstName = req.body.firstName;
-     var lastName  = req.body.lastName;
-     var address   = req.body.address;
-     var city      = req.body.city;
-     var state     = req.body.state;
-     var zipcode   = req.body.zipcode;
-     
-     var newForm  = {
-         firstName:firstName, 
-         lastName:lastName,
-         address:address,
-         city:city,
-         state:state,
-         zipcode:zipcode
-        };
+    var id = req.body.id.trim() === '' ? null : req.body.id; 
+    // var id = req.body.id.trim();
+    
+    
 
-    //Adding to the database
-    Form.create(newForm, function(error, post) {
-        console.log("data added");
-    });
+
+    var formData = {
+        'firstName': req.body.firstName,
+        'lastName': req.body.lastName,
+        'address': req.body.address,
+        'city': req.body.city,
+        'state': req.body.state,
+        'zipcode': req.body.zipcode,
+    };
+
+    if (id === null ) {
+        Form.create(formData, function (err, doc) {
+            if (err) { console.log('ERROR:', err); }
+            console.log('CREATED DOCUMENT:', doc);
+            return res.send(doc);
+        });
+    } else {
+        Form.update({"_id": id}, formData, function(err, doc) {
+            if (err) { console.log('ERROR:', err); }
+            console.log(doc);
+        });
+    }
 
 });
 
 
 
 
+
+   
+
+//  Form.find({"_id":id}, function(err, foundData){
+     
+//     //  console.log("FOUND DATA",foundData); 
+//     var createID = foundData[0]._id.toString();
+//     console.log(createID, foundData);
+//     console.log(id);
+    
+//     console.log('createID TYPE:', typeof(createID));
+//     console.log('id TYPE:', typeof(id));
+    
+//     if(createID === id){
+//         console.log("exists")
+//     }else{
+//         console.log("does not exist")
+//     }
+    
+//  });
+
+
+//     var update = {
+//         "firstName":firstName
+//     }
+
+//  Form.update({}, update, {upsert: true}, function(err, upatedData) {
+ 
+//  console.log(upatedData)
+//   // raw will contain updatedExisting and the inserted item _id (if applicable)
+
+     
+//  });
+
+
+
+// });
 //LISTEN
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("SERVER STARTED");
--- workspace/public/auto_save.js
+++ workspace/public/auto_save.js
@@ -8,28 +8,59 @@ $('form input').on('input', function() {
        
        // Runs evry 3 second  
         saveToDB();
-    }, 3000);
+    }, 1000);
 });
 
-//Save to the db usign AJAX call
+
+
+// Serialize the form to JSON object
+$.fn.serializeObject = function(){
+        
+     var jsonObject = {};
+     var formSerializeArray = this.serializeArray(); //serialize the form
+    
+     // for every item in the serizalize array
+      $.each(formSerializeArray, function() {
+          
+        if (jsonObject[this.name] !== undefined) {
+                alert("UNDEFINED");
+        } else {
+            jsonObject[this.name] =   this.value ;
+        }
+    });
+    //reutrn full JSON object
+    return jsonObject;
+
+    console.log(jsonObject);
+};
+
+
+
+
+// Save to the db usign AJAX call
 function saveToDB() {
     
-    //save to db message
-    console.log('Saving to the db');
-    
-    var form = $("#form");
-    
-    //AJAX call
+    var form = $("form");
+    var idInput = $('#id');
+     
+
+    // AJAX call
     $.ajax({
-        url: '/',    //POST path
+        url: '/',                     // POST path
         type: "POST",
-        data: form.serialize(), //serialize the form as JSON
+        data: form.serializeObject(), // serialize the form as JSON
         success: function (data) {
-              //if the data is posted, show sucess message
-            console.log('Successfully posted!');  
+            // If the data is posted, show sucess message
+            console.log('Successfully posted!'); 
+            console.log('OBJECT ID:', data._id); 
+            idInput.val(data._id); //capture POST request id
         },
         error: function (error) {
             console.log("ERROR:", error);
         }
     });
- }
+
+}
+
+
+
